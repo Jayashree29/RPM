3d42e167cd9be2f3fe2e117eb948c9180f92269c
Jayashree29 <30969408+Jayashree29@users.noreply.github.com>
2019-06-02 19:46:54 +0530

    Add files via upload

---
+++ RPM_project.ino
@@ -0,0 +1,29 @@
+int sp=8;
+int x;
+int count=0;
+float RPM;
+unsigned int time;
+void setup()
+{
+  pinMode(sp,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  x=digitalRead(sp);
+  if(x==HIGH)
+  {
+    count++;
+   }
+   while(count>0)
+   {
+    time=millis();
+     RPM=1000/time;
+     break;
+   }
+   Serial.println(time);
+   Serial.println(RPM);
+   delay(250);
+   
+}
+
